c3aeddb9241e1ee7893e9489f73d64c095862317
yanping <yanping.chen@cos.name>
2013-11-15 17:11:29 +0800

    rebuild

--- json/news.js
+++
@@ -1,4 +0,0 @@
----
-layout: nil
----
-var newspost = [{% for post in site.categories.news %}['{{post.title}}', '{{site.base_url}}{{post.url}}', '{{ post.date| date:"%Y-%m-%d"}}']{% if forloop.last == false %}, {% endif %}{% endfor %}];
